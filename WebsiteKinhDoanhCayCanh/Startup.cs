6209c527240f1ef2f1fce0bf45f456e6f59823e2
unknown <duongquocan222@gmail.com>
2022-10-17 15:53:53 +0700

    create project

---
+++ WebsiteKinhDoanhCayCanh/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebsiteKinhDoanhCayCanh.Startup))]
+namespace WebsiteKinhDoanhCayCanh
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
